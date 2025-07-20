075504d9b30f9810ab21fb5feab66041aae1f2df
RishantGod <godararishant@gmail.com>
2025-07-21 00:18:58 +0530

    trying another attempt by adding build object inside the vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SDG_Dashboard_v2/', // This should match your repository name
+  base: '/SDG_Dashboard_v2/', // This now matches your homepage
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
