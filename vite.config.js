58cc4d17b50adac0063f487dcff8a94656158e3d
RishantGod <godararishant@gmail.com>
2025-07-23 20:00:44 +0530

    Github pages try1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SDG_Dashboard_v2/', // This now matches your homepage
+  base: '/SDG_Dashboard_v2', // This now matches your homepage
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
